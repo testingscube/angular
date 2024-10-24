3b4d47a7da834b66f5360822567a1ed20eda8869
sshankar <sshankar@scube.scube>
2024-10-24 16:48:16 -0400

    generating random status in constructor

--- deep-into-component-angular/src/app/dashboard/server-status/server-status.component.ts
+++ deep-into-component-angular/src/app/dashboard/server-status/server-status.component.ts
@@ -8,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrl: './server-status.component.css',
 })
 export class ServerStatusComponent {
-  currentStatus: string = 'online';
+  currentStatus: 'online' | 'offline' | 'unknown' = 'online';
+
+  constructor() {
+    setInterval(() => {
+      const randomNumber = Math.random(); // 0 - 0.999999999999
+      if (randomNumber < 0.5) this.currentStatus = 'online';
+      else if (randomNumber < 0.9) this.currentStatus = 'offline';
+      else this.currentStatus = 'unknown';
+    }, 5000);
+  }
 }
