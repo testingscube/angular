3a7db81f8809d91b7833de3f651f0b56d206f4ce
sshankar <sshankar@scube.scube>
2024-10-24 16:59:48 -0400

    Adding code from constructor to ngOnInit right place to set timer

--- deep-into-component-angular/src/app/dashboard/server-status/server-status.component.ts
+++ deep-into-component-angular/src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,10 +7,14 @@ import { Component } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css',
 })
-export class ServerStatusComponent {
+export class ServerStatusComponent implements OnInit{
   currentStatus: 'online' | 'offline' | 'unknown' = 'online';
 
-  constructor() {
+  constructor() {} // only use for basic initilization
+
+
+  // Although ngOnInit not need OnInit implementation you shold do it to avoid typo.
+  ngOnInit() {
     setInterval(() => {
       const randomNumber = Math.random(); // 0 - 0.999999999999
       if (randomNumber < 0.5) this.currentStatus = 'online';
