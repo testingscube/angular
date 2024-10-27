bea85f4ef05be687a7b3366fc7ba7d308f8e8ed1
sshankar <sshankar@scube.scube>
2024-10-27 17:16:12 -0400

    DestroyRef instead of using OnDestroy

--- deep-into-component-angular/src/app/dashboard/server-status/server-status.component.ts
+++ deep-into-component-angular/src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DestroyRef, OnDestroy, OnInit, inject } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,19 +7,38 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css',
 })
-export class ServerStatusComponent implements OnInit{
+export class ServerStatusComponent implements OnInit {
   currentStatus: 'online' | 'offline' | 'unknown' = 'online';
+  // private interval?: ReturnType<typeof setInterval>;
+
+  //after Angular-16
+  private destroyRef = inject(DestroyRef);
 
   constructor() {} // only use for basic initilization
 
-
   // Although ngOnInit not need OnInit implementation you shold do it to avoid typo.
   ngOnInit() {
-    setInterval(() => {
+    // this.interval =
+    const interval = setInterval(() => {
       const randomNumber = Math.random(); // 0 - 0.999999999999
       if (randomNumber < 0.5) this.currentStatus = 'online';
       else if (randomNumber < 0.9) this.currentStatus = 'offline';
       else this.currentStatus = 'unknown';
     }, 5000);
+
+
+    // This feature after angular 16, this will add an 
+    // listener which will be executed on destroy of component.
+    this.destroyRef.onDestroy(() => {
+      clearTimeout(interval);
+    });
   }
+
+  // More latest way of doing this is provided by angular. 
+  // Instead 
+  // ngOnDestroy(): void {
+  //   clearTimeout(this.interval);
+  // }
+
+
 }
